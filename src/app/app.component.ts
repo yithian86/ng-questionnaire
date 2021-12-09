0f1d986f7dede519a3e32c5aa285ec0465c677cf
acinelli <alex.cinelli@objectway.com>
2021-12-09 20:58:00 +0100

    fix(app.component): updated header text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,14 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'angular-questionnaire';
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {}
+
+  get pageTitle() {
+    return this.router?.url?.substring(1, this.router.url.length);
+  }
+
 }
